624a90461f2530d49b69f2e12c9f5bb58ef645c9
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-04-27 01:25:13 +0530

    Basic Backen Setup in Express

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/v1/auth",authRoutes)
 
 export default app;
---
+++ Server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup= async (req,res)=>{
+    await res.send("Sign Up Route");
+};
+
+export const logout= async (req,res)=>{
+    await res.send("Logout Route");
+};
+
+export const login= async (req,res)=>{
+    await res.send("Login Route");
+};

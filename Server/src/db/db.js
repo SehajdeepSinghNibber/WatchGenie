282732925c4ab6aa71d219e22536be4b9932b757
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-04-27 00:49:32 +0530

    Initial commit

---
+++ Server/src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const connectDB = async ()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/watchgenie`)
+        console.log(`db connected`)
+    } catch (error) {
+        console.log(`db not connexted`);
+        console.log(error)
+        process.exit(1);
+    }
+}
+
+export default connectDB;

9e62840a1132f6f256d4fb09fec506c40f63c591
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-05-02 02:18:18 +0530

    added trailers route

--- Server/src/controllers/movie.controller.js
+++ Server/src/controllers/movie.controller.js
@@ -16,3 +16,26 @@ export async function getTrendingMovie(req,res){
         })
     }
 }
+
+export async function getMovieTrailers(req,res){
+    const { id } = req.params;
+    try {
+        const data = await fetchFromTmdb(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`)
+
+        res.json({
+            success:true,
+            trailers: data.results
+        })
+
+    } catch (error) {
+        if (error.message.includes("404")){
+            return res.status(404).send(null);
+        }
+        else{
+            return res.status(500).json({
+                success:false,
+                message: "Internal Server Error"
+            })
+        }
+    }
+}
